5a113752b729ce725b624f023970b54d5e0c783b
zuohw1 <zuohongwei1986@163.com>
2018-09-17 18:58:44 +0800

    接口返回实体

--- src/main/java/cn/chinaunicom/utils/MessageResponse.java
+++ src/main/java/cn/chinaunicom/utils/MessageResponse.java
@@ -5,16 +5,16 @@ import io.swagger.annotations.ApiModelProperty;
 
 /**
  * **************************************
- * 描述: 接口错误信息实体类 
+ * 描述: 接口返回信息实体类 
  * @ClassName: cn.chinaunicom.utils.MessageResponse 
  * @author zhw 
  * @version V1.0
 ***************************************
  */
-@ApiModel(value = "MessageResponse", description = "错误响应实体")
+@ApiModel(value = "MessageResponse", description = "接口响应实体")
 public class MessageResponse {
 
-    @ApiModelProperty(value = "接口错误信息")
+    @ApiModelProperty(value = "接口返回信息")
     private String msg;
 
     public String getMsg() {
