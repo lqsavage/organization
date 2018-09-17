f78c1237c55798f000c33ad988483df2e3425d18
zuohw1 <zuohongwei1986@163.com>
2018-09-17 18:50:02 +0800

    模板文件

---
+++ src/main/java/cn/chinaunicom/service/IHrService.java
@@ -0,0 +1,15 @@
+package cn.chinaunicom.service;
+
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * **************************************
+ * 描述:项目Service接口基类 
+ * @ClassName: cn.chinaunicom.service.IHrService 
+ * @author zhw 
+ * @version V1.0
+***************************************
+ */
+public interface IHrService<T> extends IService<T>{
+
+}
