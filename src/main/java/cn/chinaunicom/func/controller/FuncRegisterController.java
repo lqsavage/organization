b56a2e7a4491508f3f18640c0caf8860cc5467fd
erickGronwall <yyh1144@163.com>
2018-10-16 10:34:12 +0800

    excel-tool

--- src/main/java/cn/chinaunicom/func/controller/FuncRegisterController.java
+++ src/main/java/cn/chinaunicom/func/controller/FuncRegisterController.java
@@ -1,8 +1,8 @@
 package cn.chinaunicom.func.controller;
 
-
 import java.util.ArrayList;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.baomidou.mybatisplus.plugins.Page;
+
 import cn.chinaunicom.func.entity.FuncRegister;
 import cn.chinaunicom.func.service.FuncRegisterService;
 import cn.chinaunicom.platform.utils.MessageResponse;
@@ -21,70 +24,83 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
-import org.springframework.web.bind.annotation.RestController;
-
 /**
- * **************************************
- * 描述: 节点注册
- * @ClassName: cn.chinaunicom.func.controller.FuncRegisterController 
- * @author zuohongwei 
+ * ************************************** 描述: 节点注册
+ * 
+ * @ClassName: cn.chinaunicom.func.controller.FuncRegisterController
+ * @author zuohongwei
  * @since 2018-09-17
- * @version V1.0
- * **************************************
+ * @version V1.0 **************************************
  */
-@Api(value = "节点注册", tags = "节点注册")
-@CrossOrigin(origins = "*", maxAge = 3600)
+@Api(
+		value = "节点注册",
+		tags = "节点注册")
+@CrossOrigin(
+		origins = "*",
+		maxAge = 3600)
 @RestController
 @RequestMapping("/funcRegister")
 public class FuncRegisterController {
 
 	@Autowired
 	FuncRegisterService service;
-	
-	@ApiOperation(value = "节点注册列表", notes = "节点注册列表", response = FuncRegister.class, httpMethod = "GET")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "x-token-code", value = "用户登录令牌", paramType = "header", dataType = "String", required = true, defaultValue = "xjMjL0m2A6d1mOIsb9uFk+wuBIcKxrg4")
-    })
-    @ApiResponses({
-            @ApiResponse(
-                    code = 200,
-                    message = "获取数据成功",
-                    response = Page.class
-            ),
-            @ApiResponse(
-                    code = 404,
-                    message = "未查询到数据"
-            )
-    })
-    @GetMapping("/list")
-    public ResponseEntity<Object> getFuncList(){
-		List<FuncRegister> list = service.getFuncRegisterList();
-        if(list==null) {
-            list = new ArrayList<FuncRegister>();
-        }
-        return new ResponseEntity<>(list, HttpStatus.OK);
-    }
-	
-	@ApiOperation(value = "保存节点注册", notes = "保存节点注册", response = MessageResponse.class, httpMethod = "POST")
-	@ApiImplicitParams({
-    @ApiImplicitParam(name = "x-token-code", value = "用户登录令牌", paramType = "header", dataType = "String", required = true, defaultValue = "xjMjL0m2A6d1mOIsb9uFk+wuBIcKxrg4")
-	})
-	@ApiResponses({
-	         @ApiResponse(
-	                 code = 200,
-	                 message = "添加成功"
-	         )
-	})
-	@PostMapping("/save")
-	public ResponseEntity<Object> saveFuncRegister(FuncRegister entity){
-		MessageResponse vo = new MessageResponse();
-        Integer i = service.saveFuncRegister(entity);
-        if(i>0) {
-        	vo.setMsg("添加成功");
-        }else {
-        	vo.setMsg("添加失败");
-        }
-        return new ResponseEntity<>(vo, HttpStatus.OK);
-    }
-}
 
+	@ApiOperation(
+			value = "节点注册列表",
+			notes = "节点注册列表",
+			response = FuncRegister.class,
+			httpMethod = "GET")
+	@ApiImplicitParams({ @ApiImplicitParam(
+			name = "x-token-code",
+			value = "用户登录令牌",
+			paramType = "header",
+			dataType = "String",
+			required = true,
+			defaultValue = "xjMjL0m2A6d1mOIsb9uFk+wuBIcKxrg4") })
+	@ApiResponses({ @ApiResponse(
+			code = 200,
+			message = "获取数据成功",
+			response = Page.class),
+			@ApiResponse(
+					code = 404,
+					message = "未查询到数据") })
+	@GetMapping("/list")
+	public ResponseEntity<Object> getFuncList() {
+		List<FuncRegister> list = service.getFuncRegisterList();
+		if (list == null)
+		{
+			list = new ArrayList<FuncRegister>();
+		}
+		return new ResponseEntity<>(list, HttpStatus.OK);
+	}
+
+	@ApiOperation(
+			value = "保存节点注册",
+			notes = "保存节点注册",
+			response = MessageResponse.class,
+			httpMethod = "POST")
+	@ApiImplicitParams({ @ApiImplicitParam(
+			name = "x-token-code",
+			value = "用户登录令牌",
+			paramType = "header",
+			dataType = "String",
+			required = true,
+			defaultValue = "xjMjL0m2A6d1mOIsb9uFk+wuBIcKxrg4") })
+	@ApiResponses({ @ApiResponse(
+			code = 200,
+			message = "添加成功") })
+	@PostMapping("/save")
+	public ResponseEntity<Object> saveFuncRegister(FuncRegister entity) {
+		MessageResponse vo = new MessageResponse();
+		Integer i = service.saveFuncRegister(entity);
+		if (i > 0)
+		{
+			vo.setMsg("添加成功");
+		} else
+		{
+			vo.setMsg("添加失败");
+		}
+		return new ResponseEntity<>(vo, HttpStatus.OK);
+	}
+
+}
